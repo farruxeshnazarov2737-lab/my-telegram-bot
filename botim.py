efdc55a59451bc0d9f1a63a04b9c7f93f7e3f718
farruxeshnazarov2737-lab <farruxeshnazarov2737@gmail.com>
2026-08-25 23:24:49 +0500

    Update botim.py

--- botim.py
+++ botim.py
@@ -1,3 +1,21 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return 'Bot ishlamoqda!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+Thread(target=run).start()
 import asyncio
 from datetime import datetime, timezone
 from aiogram import Bot, Dispatcher, types, F
