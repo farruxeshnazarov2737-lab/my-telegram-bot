ec24cb86ec37373b84c1c99270eefff2c39f9289
farruxeshnazarov2737-lab <farruxeshnazarov2737@gmail.com>
2026-08-26 08:21:57 +0500

    Update botim.py

--- botim.py
+++ botim.py
@@ -434,7 +434,9 @@ async def complete_account_deletion(message: types.Message, state: FSMContext, u
         await state.clear()
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
